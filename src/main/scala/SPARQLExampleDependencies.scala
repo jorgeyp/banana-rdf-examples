979c55ce592dea0a10100835a5bb5beedcfecb2d
Jorge Yagüe París <jorgeyp@gmail.com>
2015-05-25 18:18:10 +0200

    Add more examples.

--- src/main/scala/SPARQLExampleDependencies.scala
+++
@@ -1,7 +0,0 @@
-import org.w3.banana.{SparqlHttpModule, SparqlOpsModule, RDFOpsModule, RDFModule}
-
-trait SPARQLExampleDependencies
-  extends RDFModule
-  with RDFOpsModule
-  with SparqlOpsModule
-  with SparqlHttpModule
---
+++ src/main/scala/eXchange2014.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by jorge on 25/5/15.
+ */
+class eXchange2014 {
+
+}
